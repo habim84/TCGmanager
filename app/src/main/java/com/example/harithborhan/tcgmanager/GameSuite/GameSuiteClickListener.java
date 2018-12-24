6f4fac7471105e6f84e79f8b7a6bd6c187a62d02
Harith Borhan <harith.borhan@mecreativestudio.com>
2018-12-25 04:59:52 +0800

    Added recycler adapter to handle game suite views.

---
+++ app/src/main/java/com/example/harithborhan/tcgmanager/GameSuite/GameSuiteClickListener.java
@@ -0,0 +1,7 @@
+package com.example.harithborhan.tcgmanager.GameSuite;
+
+import android.view.View;
+
+public interface GameSuiteClickListener {
+    void onClick(View view, int position);
+}
--- app/src/main/java/com/example/harithborhan/tcgmanager/GameSuite/GameSuiteRecyclerAdapter.java
+++ app/src/main/java/com/example/harithborhan/tcgmanager/GameSuite/GameSuiteRecyclerAdapter.java
@@ -1,6 +1,12 @@
 package com.example.harithborhan.tcgmanager.GameSuite;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.harithborhan.tcgmanager.R;
 
 import java.util.List;
 
@@ -10,4 +16,48 @@ public class GameSuiteRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.
 
     private List<GameSuiteObject> gameSuites;
 
+    private GameSuiteClickListener clickListener;
+
+    public GameSuiteRecyclerAdapter(List<GameSuiteObject> gameSuites) {
+        this.gameSuites = gameSuites;
+    }
+
+    public void setOnClickListener(GameSuiteClickListener clickListener) {
+        this.clickListener = clickListener;
+    }
+
+    public class GameSuiteHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView gameSuiteName;
+
+        public GameSuiteHolder(View view) {
+            super(view);
+            gameSuiteName = view.findViewById(R.id.row_game_suite_name);
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (clickListener != null) {
+                clickListener.onClick(view, getAdapterPosition());
+            }
+        }
+    }
+
+    @Override
+    public GameSuiteHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.row_game_suite, parent, false);
+        return new GameSuiteHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        GameSuiteObject gameSuite = this.gameSuites.get(position);
+        String mGameSuiteName = gameSuite.getGameSuiteName();
+        GameSuiteHolder cHolder = (GameSuiteHolder) holder;
+        cHolder.gameSuiteName.setText(mGameSuiteName);
+    }
+
+    @Override
+    public int getItemCount() { return this.gameSuites.size(); }
 }
