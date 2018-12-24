92dffbc3dcff5d9e512ff24fa1ff5a5f5feb4ddb
Harith Borhan <harith.borhan@mecreativestudio.com>
2018-12-25 05:20:53 +0800

    Linked game suite recycler adapter to TCGhome activity.

--- app/src/main/java/com/example/harithborhan/tcgmanager/TCGHome.java
+++ app/src/main/java/com/example/harithborhan/tcgmanager/TCGHome.java
@@ -2,23 +2,34 @@ package com.example.harithborhan.tcgmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
+import com.example.harithborhan.tcgmanager.GameSuite.GameSuiteClickListener;
 import com.example.harithborhan.tcgmanager.GameSuite.GameSuiteObject;
+import com.example.harithborhan.tcgmanager.GameSuite.GameSuiteRecyclerAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class TCGHome extends AppCompatActivity {
+public class TCGHome extends AppCompatActivity implements GameSuiteClickListener {
 
     private final static String TAG = "TCGHome";
 
     private List<GameSuiteObject> gameSuiteList = new ArrayList<>();
+    private GameSuiteRecyclerAdapter gameSuiteAdapter;
+    private RecyclerView gameSuiteRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tcghome);
+
+        // Set UI elements
+        gameSuiteRecyclerView = this.findViewById(R.id.recycler_game_suite);
 
         // Setup toolbar
         Toolbar homeToolbar = findViewById(R.id.tcg_home_toolbar);
@@ -29,6 +40,16 @@ public class TCGHome extends AppCompatActivity {
         }
 
         // Setup Recycler Adapter for the list of Game Suites
+        gameSuiteAdapter = new GameSuiteRecyclerAdapter(gameSuiteList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        gameSuiteRecyclerView.setLayoutManager(mLayoutManager);
+        gameSuiteRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        gameSuiteRecyclerView.setAdapter(gameSuiteAdapter);
+        gameSuiteAdapter.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View view, int position) {
+        // TODO: Setup on click action on one of the recycler view item.
     }
 }
